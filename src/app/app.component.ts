f88cea5c123a5096b92701419c24ab0216f22b15
coerschkes <christian.oerschkes@hotmail.de>
2023-05-10 08:50:13 +0200

    implement basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  currentFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.currentFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() navigationEmitter = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.navigationEmitter.emit(feature);
+  }
 }
