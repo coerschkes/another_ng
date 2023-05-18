ad43318c7f8d61eb8f9ae94a25e61edda3827d7f
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-18 20:30:55 +0200

    display ingredients in recipe-edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -41,24 +41,43 @@ export class RecipeEditComponent implements OnInit {
     this.editMode = false;
   }
 
-  private initForm() {
-    this.editForm = new FormGroup({
-      name: new FormControl(null, Validators.required),
-      imagePath: new FormControl(null, Validators.required),
-      description: new FormControl(null, Validators.required),
-    });
-
-    if (this.editMode) {
-      this.populateForm();
-    }
+  get controls() {
+    return (this.editForm.get('ingredients') as FormArray).controls;
   }
 
-  private populateForm() {
+  //*ngFor="let ingredientCtrl of controls; let i = index"
+
+  private initForm() {
     const editRecipe = this.recipeService.getRecipe(this.id);
-    this.editForm.setValue({
-      name: editRecipe.name,
-      imagePath: editRecipe.imagePath,
-      description: editRecipe.description,
+    let name = '';
+    let imagePath = '';
+    let description = '';
+    let ingredients = new FormArray([]);
+
+    if (this.editMode) {
+      name = editRecipe.name;
+      imagePath = editRecipe.imagePath;
+      description = editRecipe.description;
+      if (editRecipe['ingredients']) {
+        for (let ingredient of editRecipe.ingredients) {
+          ingredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
+            })
+          );
+        }
+      }
+    }
+
+    this.editForm = new FormGroup({
+      name: new FormControl(name, Validators.required),
+      imagePath: new FormControl(imagePath, Validators.required),
+      description: new FormControl(description, Validators.required),
+      ingredients: new FormArray(ingredients.controls),
     });
   }
 
