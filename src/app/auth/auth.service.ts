72c39768ae6b833a9cdcc41e446208d33577205c
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-19 14:22:56 +0200

    implement autologin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -37,6 +37,27 @@ export class AuthService {
       .pipe(tap(this.handleAuthentication.bind(this)));
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+    if (loadedUser.token) {
+      this.userSubject.next(loadedUser);
+    }
+  }
+
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(firebaseLoginUrl, {
@@ -64,6 +85,7 @@ export class AuthService {
       expirationDate
     );
     this.userSubject.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
