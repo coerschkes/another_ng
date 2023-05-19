606de237632bce1fcdd8fa9fcfbd3f80c155b3dc
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-19 10:49:31 +0200

    fix save method on edit recipe

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,7 +2,12 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 export const RecipesResolverService: ResolveFn<Recipe[]> = () => {
+  const recipes = inject(RecipeService).getRecipes();
+  if (recipes.length > 0) {
+    return recipes;
+  }
   return inject(DataStorageService).fetchData();
 };
