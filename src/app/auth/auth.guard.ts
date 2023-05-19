af8beb9e64b1dcdee7e8f7026349f9d3d7b4229d
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-19 14:43:25 +0200

    implement an auth guard to protect the authenticated routes

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+export const AuthGuard: CanActivateFn = () => {
+  const router: Router = inject(Router);
+  const authService = inject(AuthService);
+  return authService.userSubject.pipe(
+    take(1),
+    map((user) => {
+      const isAuth = !!user;
+      if (isAuth) {
+        return true;
+      }
+      return router.createUrlTree(['/auth']);
+    })
+  );
+};
