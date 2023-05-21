59e872996aee317a7037ca454b669211d413403c
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-21 21:00:04 +0200

    lazy load the auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AuthModule,
     SharedModule,
   ],
   providers: [
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { AuthComponent } from './auth.component';
 
 const routes: Routes = [
   {
-    path: 'auth',
+    path: '',
     component: AuthComponent,
   },
 ];
